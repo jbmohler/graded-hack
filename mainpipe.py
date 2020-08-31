a78421b38679890c709b45b38461f28568312d6e
Joel B. Mohler <joel@kiwistrawberry.us>
2020-08-31 14:45:47 -0400

    add dockerfile and simple factor

--- mainpipe.py
+++ mainpipe.py
@@ -1,9 +1,16 @@
 import conducto as co
 
+def factor(n):
+    import sympy
+    print(sympy.factorint(int(n)))
+
 
 def test() -> co.Serial:
-    output = co.Serial()
+    image = co.Image(dockerfile='docker/Dockerfile', copy_repo=True)
+
+    output = co.Serial(image=image)
     output["node1"] = co.Exec("echo testing")
+    output["factor"] = co.Exec(factor, 91235)
     return output
 
 
