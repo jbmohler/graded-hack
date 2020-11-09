afb28d4450e3be8e2eefe739bc02ae0cf92f233e
Joel B. Mohler <joel@kiwistrawberry.us>
2020-11-09 13:45:37 -0500

    style checks in CI (#14)
    formatting & style checks in CI

--- pipeline.py
+++ pipeline.py
@@ -8,14 +8,21 @@ import conducto as co
 def pr(branch) -> co.Parallel:
     # Make a Docker image, based on python:alpine, with the whole repo and the contents
     # of the given branch.
-    image = co.Image("python:alpine", copy_repo=True, copy_branch=branch)
+    image = co.Image("python:3.8", copy_repo=True, copy_branch=branch)
 
     # Using that Docker image, run three commands in parallel to interact with the
     # repo's files.
     with co.Parallel(image=image) as root:
         co.Exec(f"echo {branch}", name="print branch")
-        co.Exec("pwd", name="print working directory")
         co.Exec("ls -la", name="list files")
+        with co.Serial(
+            image=image,
+            name="checks",
+            container_reuse_context=co.ContainerReuseContext.NEW,
+        ):
+            co.Exec("pip install black flake8", name="install")
+            co.Exec("black *.py --check", name="formatting")
+            co.Exec("flake8 *.py", name="linting")
 
     co.git.apply_status_all(root)
 
